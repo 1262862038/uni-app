29b7e1f088792d55a75a44ac33939e162e6d5a20
章祝兵 <7928290+zhang_zhubing@user.noreply.gitee.com>
2020-08-31 20:01:03 +0800

    zzb31

---
+++ pages/home-detail/home-detail.vue
@@ -0,0 +1,144 @@
+<template>
+	<view class="detail">
+		<view class="detail-title">{{formData.title}}</view>
+		<view class="detail-header">
+			<view class="detail-header_logo">
+				<image :src="formData.author.avatar" mode="aspectFill"></image>
+			</view>
+			<view class="detail-header_content">
+				<view class="detail-header_content-title">{{formData.author.name}}</view>
+				<view class="detail-header_content-info">
+					<text>{{formData.create_time}}</text>
+					<text>{{formData.browse_count}}浏览</text>
+					<text>{{formData.thumbs_up_count}}赞</text>
+				</view>
+			</view>
+		</view>
+		<view class="detail-content">
+			详情数据
+		</view>
+		<view class="detail-bottom">
+			<view class="detail-bottom_input">
+				<text>谈谈你的看法</text>
+				<uni-icons type="compose" size="16" color="#f07373"></uni-icons>
+			</view>
+			<view class="detail-bottom_icons">
+				<view class="detail-bottom_icons-box">
+					<uni-icons type="chat" size="22" color="#f07373"></uni-icons>
+					
+				</view>
+				<view class="detail-bottom_icons-box">
+					<uni-icons type="heart" size="22" color="#f07373"></uni-icons>
+				</view>
+				<view class="detail-bottom_icons-box">
+					<uni-icons type="hand-thumbsup" size="22" color="#f07373"></uni-icons>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				formData: {}
+			};
+		},
+		onLoad(query) {
+			console.log('query', JSON.parse(query.params))
+			this.formData = JSON.parse(query.params)
+		}
+	}
+</script>
+
+<style lang="scss">
+.detail {
+	padding: 15px 0;
+	padding-bottom: 54px;
+	.detail-title {
+		padding: 0 15px;
+		font-size: 18px;
+		font-weight: bold;
+		color: #333;
+	}
+	.detail-header {
+		display: flex;
+		align-items: center;
+		margin-top: 10px;
+		padding: 0 15px;
+		.detail-header_logo {
+			flex-shrink: 0;
+			width: 40px;
+			height: 40px;
+			border-radius: 50%;
+			overflow: hidden;
+			image {
+				width: 100%;
+				height: 100%;
+			}
+		}
+		.detail-header_content {
+			width: 100%;
+			padding-left: 10px;
+			display: flex;
+			flex-direction: column;
+			justify-content: space-between;
+			font-size: 12px;
+			.detail-header_content-title {
+				font-size: 14px;
+				color: #333;
+			}
+			.detail-header_content-info {
+				color: #999;
+				text {
+					margin-right: 10px;
+				}
+			}
+		}
+	}
+	.detail-content {
+		height: 1000px;
+		border: 1px solid red;
+	}
+	.detail-bottom {
+		position: fixed;
+		bottom: 0;
+		left: 0;
+		display: flex;
+		align-items: center;
+		width: 100%;
+		height: 44px;
+		border-top: 1px solid #f5f5f5;
+		background-color: #fff;
+		box-sizing: border-box;
+		.detail-bottom_input {
+			display: flex;
+			align-items: center;
+			justify-content: space-between;
+			margin-left: 10px;
+			padding: 0 10px;
+			width: 100%;
+			height: 30px;
+			border: 1px solid #ddd;
+			border-radius: 5px;
+			text {
+				color: #999;
+				font-size: 14px;
+			}
+		}
+		.detail-bottom_icons {
+			display: flex;
+			flex-shrink: 0;
+			padding: 0 10px;
+			.detail-bottom_icons-box {
+				position: relative;
+				display: flex;
+				align-items: center;
+				justify-content: center;
+				width: 44px;
+			}
+		}
+	}
+}
+</style>
